d1b7589bf20b438a62851a27be622c1592b15681
Mitch Toran <mitchellht34@gmail.com>
2024-06-10 16:32:27 -0400

    remove ebug

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -7,9 +7,5 @@ import Article from "../components/Article";
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
 
-  const element = screen.queryByText("please pass this test");
-
-  screen.debug(element);
-
-  expect(element).toBeInTheDocument();
+  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 });
--- src/components/Article.js
+++ src/components/Article.js
@@ -3,7 +3,6 @@ import React from "react";
 function Article() {
   return (
     <div>
-      {/* please pass this test */}
       please pass this test
     </div>
   );
